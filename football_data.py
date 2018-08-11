2e342ad3ae0bf760885266fdb84cfeafda2b8391
Sean Merricks <seanmerricks@gmail.com>
2018-08-11 19:19:53 +0100

    Added 'Year On Year' stats panel and expanded data set to three years.

--- football_data.py
+++ football_data.py
@@ -46,24 +46,49 @@ app.layout = html.Div([
         html.Hr(style={'margin':0, 'padding':0, 'border':'0', 'backgroundColor':colours['line_colour'], 'height':'1px'}),
     ], style={'backgroundColor':colours['background_white']}),
     html.Div([
-        # controls div
         html.Div([
+            # single team controls div
+            html.H2('Single Team Analysis', style={'font-size':'1.4em'}),
             html.Div([
-                html.H2('Choose team.', style={}),
-                dcc.Dropdown(id='team-picker', options=teams, value=''), #value=teams[0]['value']
+                html.H2('Choose team.', style={'font-size':'1.2em'}),
+                dcc.Dropdown(id='team-picker', options=teams, value=teams[0]['value']),
             ]),
             html.Div([
-                html.H2('Choose season.', style={}),
-                dcc.Dropdown(id='season-picker', options=seasons, value=''), #seasons[0]['value']
+                html.H2('Choose season.', style={'font-size':'1.2em'}),
+                dcc.Dropdown(id='season-picker', options=seasons, value=seasons[0]['value']),
             ]),
+            html.Hr(style={'margin':'20px 0px', 'padding':0, 'border':'0', 'backgroundColor':colours['line_colour'], 'height':'1px'}),
+            # team stats over years controls div
+            html.H2('Team Year On Year', style={'font-size':'1.4em'}),
+            html.Div([
+                html.H2('Choose team.', style={'font-size':'1.2em'}),
+                dcc.Dropdown(id='team-picker1', options=teams, value=''), #value=teams[0]['value']
+            ]),
+            html.Div([
+                html.H2('Choose season.', style={'font-size':'1.2em'}),
+                dcc.Checklist(id='season-picker1', options=seasons, values='', labelStyle={'margin':'10px 0px', 'display':'block'}), #seasons[0]['value']
+            ]),
+            html.Hr(style={'margin':'20px 0px', 'padding':0, 'border':'0', 'backgroundColor':colours['line_colour'], 'height':'1px'}),
         ], style={'width':'15%', 'borderRight':'1px solid #E1E1E1', 'padding':'0px 20px', 'float':'left', 'display':'inline-block', 'backgroundColor':colours['background_grey']}),
-        # data and graphs
+        # single team stats
         html.Div([
+            html.H2('Single Team Analysis', style={'font-size':'1.2em'}),
             html.Div([
                 dcc.Graph(id='graph'),
-            ], style={'width':'60%', 'borderRight':'1px solid #E1E1E1', 'float':'left', 'display':'inline-block'}),
+            ], style={'width':'60%', 'float':'left', 'display':'inline-block', 'borderRight':'1px solid #E1E1E1', 'marginBottom':'20px', 'paddingRight':'20px'}),
             html.Div([
                 dcc.Graph(id='table'),
+            ], style={'width':'35%', 'marginTop':'0px', 'float':'left', 'display':'inline-block'}),
+            html.P('',style={'clear':'both'}),
+        ], style={'float':'left', 'width':'80%', 'borderBottom':'1px solid #E1E1E1', 'marginLeft':'20px'}),
+        html.P('',style={'clear':'both'}),
+        # team stats year over year
+        html.Div([
+            html.Div([
+                dcc.Graph(id='graph1'),
+            ], style={'width':'60%', 'borderRight':'1px solid #E1E1E1', 'float':'left', 'display':'inline-block'}),
+            html.Div([
+                dcc.Graph(id='table1'),
             ], style={'width':'35%', 'marginTop':'0px', 'float':'left', 'display':'inline-block'}),
             html.P('',style={'clear':'both'}),
         ], style={'float':'left', 'width':'80%'}),
@@ -165,6 +190,53 @@ def update_team_table(chosen_team, chosen_season):
     return {'data' : [trace],
             'layout':go.Layout(title= str(chosen_team) + ' | ' + chosen_season + ' Season' + ' | Games Played: ' + str(games_played))
     }
+
+@app.callback(Output('graph1', 'figure'),
+              [Input('team-picker1', 'value'),
+               Input('season-picker1', 'values')])
+def update_team_graph(chosen_team, chosen_season):
+    traces = []
+    for season in chosen_season:
+        # filter data on given team
+        filtered_df_home = df[(df['HomeTeam']== chosen_team) & (df['Season']== season)]
+        filtered_df_away = df[(df['AwayTeam']== chosen_team) & (df['Season']== season)]
+
+        games_played = filtered_df_home['HomeTeam'].count() + filtered_df_away['AwayTeam'].count()
+
+        goals_for_home = sum(filtered_df_home['FTHG'])
+        goals_against_home = sum(filtered_df_home['FTAG'])
+        yellow_cards_home = sum(filtered_df_home['HY'])
+        red_cards_home = sum(filtered_df_home['HR'])
+
+        goals_for_away = sum(filtered_df_away['FTAG'])
+        goals_against_away = sum(filtered_df_away['FTHG'])
+        yellow_cards_away = sum(filtered_df_away['AY'])
+        red_cards_away = sum(filtered_df_away['AR'])
+
+        points_home = []
+        for game in filtered_df_home['FTR']:
+            if game == 'H':
+                points_home.append(3)
+            elif game == 'D':
+                points_home.append(1)
+        points_away = []
+        for game in filtered_df_away['FTR']:
+            if game == 'A':
+                points_away.append(3)
+            elif game == 'D':
+                points_away.append(1)
+
+        traces.append(go.Bar(x = ['Points', 'Goals For', 'Goals Against', 'Yellow Cards', 'Red Cards'],
+                             y = [sum(points_home + points_away), goals_for_home + goals_for_away, goals_against_home + goals_against_away, yellow_cards_home + yellow_cards_away, red_cards_home + yellow_cards_away],
+                             name = season))
+
+    return {'data' : traces,
+            'layout':go.Layout(title= str(chosen_team),
+            xaxis={'title':'Statistic', 'type':'category'},
+            yaxis={'title':'Value'},
+            barmode='group'),
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
